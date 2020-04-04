ca156653552d37d8a15657fa0ac9b6565ed3dc95
Malcolm Katzenbach <mkatzenbach8@gmail.com>
2020-04-04 19:37:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,11 +18,11 @@ def move(array, index, value)
 end
 
 def position_taken?(board, index)
-  board[index] == "" || board[index] == " " || board[index] == nil
+  board[index] != " "
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && position_taken?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
